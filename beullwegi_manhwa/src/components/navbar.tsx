238610416c8b5f5e33478bfd7be9ad922880ab0f
shiüo <shiueo.csh@gmail.com>
2023-05-27 20:27:12 +0900

    r

--- beullwegi_manhwa/src/components/navbar.tsx
+++ beullwegi_manhwa/src/components/navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <img src="black.png" />
             </div>
           </label>
           <ul
