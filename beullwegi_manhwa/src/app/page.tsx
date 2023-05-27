496a76954805fa34c3c2c1098aeb194a2d05c899
shiüo <shiueo.csh@gmail.com>
2023-05-27 20:11:57 +0900

    test

--- beullwegi_manhwa/src/app/page.tsx
+++ beullwegi_manhwa/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Footer from "../components/footer";
-import Hero from "../components/hero";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Footer />
     </>
