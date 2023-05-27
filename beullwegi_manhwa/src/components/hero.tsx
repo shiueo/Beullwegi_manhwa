42ec5d55a0ad8c06e7be258fe02274ac39666700
shiüo <shiueo.csh@gmail.com>
2023-05-27 20:29:20 +0900

    Update hero.tsx

--- beullwegi_manhwa/src/components/hero.tsx
+++ beullwegi_manhwa/src/components/hero.tsx
@@ -9,10 +9,10 @@ export default function Hero() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-lg">
-          <h1 className="mb-5 text-5xl font-bold">블뤠기의 만화방</h1>
-          <p className="mb-5">설명 주세요</p>
+          <h1 className="mb-5 text-5xl font-bold">블뤠기의 만화방입니다.</h1>
+          <p className="mb-5">입장 시 커피 한 잔을 내셔야 합니다.</p>
           <button className="btn btn-primary">
-            눌러도 아직 아무것도 안돼요
+            커피를 내놓으란 말이다.
           </button>
         </div>
       </div>
