72de09f9d0fd7380f9d2dc67b30cf4c5a5a3e02d
shiüo <shiueo.csh@gmail.com>
2023-05-27 20:22:55 +0900

    e

--- beullwegi_manhwa/src/components/footer.tsx
+++ beullwegi_manhwa/src/components/footer.tsx
@@ -1,11 +1,13 @@
 export default function Footer() {
-    return (
-      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-        <div className="items-center grid-flow-col">
-          <p> For <span style={{color: "#FF0A54"}}>블뤠기</span> By <span style={{color: "#FF0A54"}}>shiüo</span></p>
-        </div>
-        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-          <a href="https://twitter.com/shiueo_csh">
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <div className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About me</a>
+        <a className="link link-hover">Contact</a>
+      </div>
+      <div>
+        <div className="grid grid-flow-col gap-4">
+         <a href="https://twitter.com/shiueo_csh">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -27,17 +29,25 @@ export default function Footer() {
               <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
             </svg>
           </a>
-          <a href="https://discord.gg/NXwVfdcygM">
+          <a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="38"
-              height="38"
+              width="24"
+              height="24"
               viewBox="0 0 24 24"
               className="fill-current"
             >
-             <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"></path>   </svg>
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+            </svg>
           </a>
         </div>
-      </footer>
-    );
-  }
+      </div>
+      <div>
+        <p>
+          For <span style={{ color: "#FF0A54" }}>블뤠기</span> by{" "}
+          <a href="https://www.youtube.com/@shiueo"><span style={{ color: "#FF0A54" }}><u>shiüo</u></span></a>, Made with ❤️
+        </p>
+      </div>
+    </footer>
+  );
+}
--- beullwegi_manhwa/src/components/hero.tsx
+++ beullwegi_manhwa/src/components/hero.tsx
@@ -1,21 +1,21 @@
 export default function Hero() {
-    return (
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: `url("shiueo_wallpaper.png")`,
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-lg">
-            <h1 className="mb-5 text-5xl font-bold">블뤠기의 만화방</h1>
-            <p className="mb-5">
-              설명 주세요
-            </p>
-            <button className="btn btn-primary">눌러도 아직 아무것도 안돼요</button>
-          </div>
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url("shiueo_wallpaper.png")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-lg">
+          <h1 className="mb-5 text-5xl font-bold">블뤠기의 만화방</h1>
+          <p className="mb-5">설명 주세요</p>
+          <button className="btn btn-primary">
+            눌러도 아직 아무것도 안돼요
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
